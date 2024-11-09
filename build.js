fba30529b2dc4a776eebcd6efe6a7ba676a01c63
designtokenses <designtokens.es@gmail.com>
2024-11-09 21:01:57 +0100

    update

--- build.js
+++ build.js
@@ -1,1 +1,11 @@
-console.log('hola mundo');
+import StyleDictionary from "style-dictionary";
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from "sd-themes-loader";
+
+register(StyleDictionary, {
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
+console.log('Hola mundo!');
